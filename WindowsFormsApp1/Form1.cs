15930dbe62d9154a32fa7a276ff382cb4f9428e8
kisric779 <kisric779@108PC16>
2025-02-18 09:19:02 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        PictureBox one = new PictureBox();
+        PictureBox two = new PictureBox();
+        PictureBox three = new PictureBox();
+        PictureBox four = new PictureBox();
+        PictureBox five = new PictureBox();
+        PictureBox six = new PictureBox();
+        PictureBox seven = new PictureBox();
+        PictureBox eight = new PictureBox();
+        PictureBox nine = new PictureBox();
+
+        Button spin = new Button();
+
+        public void Setup() 
+        { 
+        
+        }
+
+    }
+}
