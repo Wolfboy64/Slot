c3e05b5fcc18ba5dc73940c76a23da13c132ecb2
Wolfboy64 <ricsi.minecraft1116@gmail.com>
2025-02-20 23:32:08 +0100

    v0.1.0

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -5,13 +5,22 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        Button plusbet1 = new Button();
+        Button plusbet2 = new Button();
+        Button plusbet3 = new Button();
+        Button zerobet = new Button();
+        Button minusbet1 = new Button();
+        Button minusbet2 = new Button();
+        Button minusbet3 = new Button();
         public Form1()
         {
             InitializeComponent();
@@ -33,9 +42,14 @@ namespace WindowsFormsApp1
         List<PictureBox> line3 = new List<PictureBox>() {seven, eight, nine};
 
         Label lbl = new Label();
+        Label result = new Label();
+
+        
+
 
         public static Button spin = new Button();
-
+        int balance = 10000;
+        int bet = 0;
         public void Setup()
         {
             // Setup the spin button
@@ -46,6 +60,16 @@ namespace WindowsFormsApp1
             Controls.Add(spin);
 
             
+
+
+
+            Controls.Add(plusbet1);
+            Controls.Add(plusbet2);
+            Controls.Add(plusbet3);
+            Controls.Add(zerobet);
+            Controls.Add(minusbet1);
+            Controls.Add(minusbet2);
+            Controls.Add(minusbet3);
 
             // Setup PictureBoxes
             SetupPictureBox(one, new Point(10, 50));
@@ -60,9 +84,75 @@ namespace WindowsFormsApp1
 
             lbl.Location = new Point(six.Location.X + 200, six.Location.Y + 35);
             lbl.AutoSize = true;
-            lbl.Text = "Teszt";
+            lbl.Text = "jelenlegi egyenleged: " + balance.ToString() + 
+                "\nJelenlegi tét: " + bet;
             Controls.Add(lbl);
+
+            result.Location = new Point(lbl.Location.X, lbl.Location.Y + 50);
+            result.AutoSize = true;
+            Controls.Add(result);
         }
+        /*private void Setup(Button button, Point location, string text, int betChange, bool reset = false)
+        {
+            // Setup the spin button
+            spin.Location = new Point(0, 0);
+            spin.AutoSize = true;
+            spin.Text = "Spin";
+            spin.Click += Spin_Click;
+            Controls.Add(spin);
+
+            // Setup bet buttons
+            SetupBetButton(plusbet1, new Point(spin.Location.X + 200, spin.Location.Y), "+1", 1);
+            SetupBetButton(plusbet2, new Point(plusbet1.Location.X + 75, plusbet1.Location.Y), "+10000", 10000);
+            SetupBetButton(plusbet3, new Point(plusbet2.Location.X + 75, plusbet2.Location.Y), "+100000", 100000);
+            SetupBetButton(zerobet, new Point(plusbet3.Location.X + 75, plusbet3.Location.Y), "0", 0, true);
+            SetupBetButton(minusbet1, new Point(zerobet.Location.X + 75, zerobet.Location.Y), "-1000", -1000);
+            SetupBetButton(minusbet2, new Point(minusbet1.Location.X + 75, minusbet1.Location.Y), "-10000", -10000);
+            SetupBetButton(minusbet3, new Point(minusbet2.Location.X + 75, minusbet2.Location.Y), "-100000", -100000);
+
+            // Setup PictureBoxes
+            SetupPictureBox(one, new Point(10, 50));
+            SetupPictureBox(two, new Point(120, 50));
+            SetupPictureBox(three, new Point(230, 50));
+            SetupPictureBox(four, new Point(10, 160));
+            SetupPictureBox(five, new Point(120, 160));
+            SetupPictureBox(six, new Point(230, 160));
+            SetupPictureBox(seven, new Point(10, 270));
+            SetupPictureBox(eight, new Point(120, 270));
+            SetupPictureBox(nine, new Point(230, 270));
+
+            lbl.Location = new Point(six.Location.X + 200, six.Location.Y + 35);
+            lbl.AutoSize = true;
+            lbl.Text = "jelenlegi egyenleged: " + balance.ToString() +
+                "\nJelenlegi tét: " + bet;
+            Controls.Add(lbl);
+
+            result.Location = new Point(lbl.Location.X, lbl.Location.Y + 50);
+            result.AutoSize = true;
+            Controls.Add(result);
+        }*/
+
+        private void SetupBetButton(Button button, Point location, string text, int betChange, bool reset = false)
+        {
+            button.Location = location;
+            button.Text = text;
+            button.Click += (sender, e) =>
+            {
+                if (reset)
+                {
+                    bet = 0;
+                }
+                else
+                {
+                    bet += betChange;
+                    if (bet < 0) bet = 0; // Prevent negative bet values
+                }
+                lbl.Text = "jelenlegi egyenleged: " + balance.ToString() +
+                    "\nJelenlegi tét: " + bet;
+            };
+            Controls.Add(button);
+        }
+
         private void SetupPictureBox(PictureBox pictureBox, Point location)
         {
             pictureBox.Size = new Size(100, 100);
@@ -119,7 +209,9 @@ namespace WindowsFormsApp1
                 line3[1].BackColor = colors[f];
                 line3[2].BackColor = colors[g];
             }
-            lbl.Text = WinCheck();
+            winmoney();
+            losemoney();
+            result.Text = WinCheck();
         }
         private string WinCheck() 
         {
@@ -129,6 +221,7 @@ namespace WindowsFormsApp1
                 if (line1[0].BackColor == line1[1].BackColor && line1[1].BackColor == line1[2].BackColor)
                 {
                     message = "You win!";
+                    
                 }
                 else 
                 {
@@ -158,6 +251,24 @@ namespace WindowsFormsApp1
                 }
             }
             return message;
+        }
+        private int losemoney()
+        {
+            int backmoney = 0;
+            if (result.Text == "You lose!") 
+            {
+                backmoney = balance - bet;
+            }
+            return backmoney;
+        }
+        private int winmoney()
+        {
+            int winmoney = 0;
+            if (result.Text == "You win!")
+            {
+                winmoney = balance + bet;
+            }
+            return winmoney;
         }
     }
 }
